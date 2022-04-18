ae908eaff335969e4d6787729ea1573a98f634cd
pawel <pawel.jan.wozniak@gmail.com>
2022-04-18 11:46:10 +0200

    FlatList component was added for hardcoded data

--- src/screens/List.js
+++ src/screens/List.js
@@ -1,12 +1,23 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { FlatList } from "react-native";
 import GlobalStyle from "../styles/GlobalStyle";
+
+const ListOfItem = [
+    {id: 1, name: 'Egg', time: 3},
+    {id: 2, name: 'Pasta', time: 7},
+    {id: 3, name: 'Rice', time: 15}
+];
 
 const List = ({ navigation }) => {
     return (
         <View style={GlobalStyle.parentContainer}>
-            <View>
-                <Text>List of itmes</Text>
+            <View style={styles.listContainer}>
+                <FlatList 
+                    data={ListOfItem}
+                    renderItem={({ item }) => <Text>{item.name}</Text>}
+                    keyExtractor={item => item.id}>
+                </FlatList>
             </View>
             <View style={GlobalStyle.btnContainer}>
                 <TouchableOpacity style={GlobalStyle.btnStyle} onPress={() => navigation.navigate('Form')}>
@@ -16,5 +27,11 @@ const List = ({ navigation }) => {
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    listContainer: {
+        flex: 1
+    }
+});
 
 export default List;
--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -7,18 +7,17 @@ export default StyleSheet.create({
         backgroundColor: '#C3D99E'
     },
     btnContainer: {
-        padding: 5
+        marginTop: 5,
+        marginBottom: 5
     },
     btnStyle: {
         backgroundColor: '#EE782D',
         color: 'white',
-        margin: 2,
         height: 40,
     },
     btnTxtStyle: {
         color: 'white',
         fontSize: 16,
-        fontWeight: 'bold',
         textAlign: 'center',
         lineHeight: 40
     }
