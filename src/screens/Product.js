88924ee15978d35be61885a99e45ab13c030c889
pawel <pawel.jan.wozniak@gmail.com>
2022-04-25 00:17:06 +0200

    Button to handle start/stop timer was added

--- src/screens/Product.js
+++ src/screens/Product.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 
 import GlobalStyle from '../styles/GlobalStyle';
@@ -6,18 +6,37 @@ import GlobalStyle from '../styles/GlobalStyle';
 const Product = ({ route, navigation }) => {
     const { details } = route.params;
 
+    const [timer, setTimer] = useState(details.time)
+    const [intervalId,setIntervalId] = useState(0);
+
     useEffect(() => {
         navigation.setOptions({title: details.name});
     },[]);
+
+    const handleClick = () => {
+        if(intervalId) {
+            clearInterval(intervalId);
+            setIntervalId(0);
+            return;
+        }
+
+        const newIntervalId = setInterval(() => {
+            setTimer(prevTimer => prevTimer - 1);
+        }, 1000);
+        setIntervalId(newIntervalId);
+    }
 
 
     return (
         <View style={GlobalStyle.parentContainer}>
             <View>
                 <Text>Name: {details.name}</Text>
-                <Text>Time: {details.time}</Text>
+                <Text>Time: {timer}</Text>
             </View>
             <View style={GlobalStyle.btnContainer}>
+                <TouchableOpacity style={GlobalStyle.btnStyle} onPress={handleClick}>
+                    <Text style={GlobalStyle.btnTxtStyle}>Start</Text>
+                </TouchableOpacity>
                 <TouchableOpacity style={GlobalStyle.btnStyle} onPress={() => navigation.goBack()}>
                     <Text style={GlobalStyle.btnTxtStyle}>Go back to the list</Text>
                 </TouchableOpacity>
