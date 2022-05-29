900931cdf33a2c446c86ba90bcaade56c5dd2743
pawel <pawel.jan.wozniak@gmail.com>
2022-05-29 17:22:16 +0200

    New styles were added to form + displaying errors was fixed

--- src/screens/Form.js
+++ src/screens/Form.js
@@ -1,34 +1,36 @@
 import React from "react";
-import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import { View, StyleSheet, Text, TextInput, TouchableOpacity } from "react-native";
 import { useForm, Controller } from 'react-hook-form';
 
 import GlobalStyle from "../styles/GlobalStyle";
 
 const Form = ({ navigation }) => {
+    const { txtInput, label, errorMsg } = styles;
 
     const { control, handleSubmit, formState: { errors }} = useForm({
         defaultValues: {
             id: Math.floor(Math.random() * 100) + 4,
             name: '',
-            minutes: 0,
-            seconds: 0
+            minutes: '0',
+            seconds: '0'
         }
     });
 
     const onSubmit = () => {
-        console.log('Everything fine');
+        console.log(errors.content);
         navigation.navigate({name: 'List'})
     };
 
     return (
-        <View>
+        <View style={GlobalStyle.parentContainer}>
              <View>
-                <Text>Name:</Text>
+                <Text style={label}>Name:</Text>
                 <Controller
                     control={control}
                     rules={{required: true}}
                     render={({ field: { onChange, onBlur, value }}) => (
                         <TextInput
+                            style={txtInput}
                             onBlur={onBlur}
                             onChangeText={onChange}
                             value={value}
@@ -36,14 +38,14 @@ const Form = ({ navigation }) => {
                     )}
                     name="name"
                 />
-                {errors.title && <Text>This is required</Text>}
-                <Text>Minutes:</Text>
+                <Text style={errorMsg}>{errors.name ? 'This is required' : ''}</Text>
+                <Text style={label}>Minutes:</Text>
                 <Controller
                     control={control}
                     rules={{required: true}}
                     render={({ field: { onChange, onBlur, value }}) => (
                         <TextInput
-                            multiline={true}
+                            style={txtInput}
                             onBlur={onBlur}
                             onChangeText={onChange}
                             value={value}
@@ -51,14 +53,14 @@ const Form = ({ navigation }) => {
                     )}
                     name="minutes"
                 />
-                {errors.content && <Text>This is required</Text>}
-                <Text>Seconds:</Text>
+                <Text style={errorMsg}>{errors.minutes ? 'This is required' : ''}</Text>
+                <Text style={label}>Seconds:</Text>
                 <Controller
                     control={control}
                     rules={{required: true}}
                     render={({ field: { onChange, onBlur, value }}) => (
                         <TextInput
-                            multiline={true}
+                            style={txtInput}
                             onBlur={onBlur}
                             onChangeText={onChange}
                             value={value}
@@ -66,18 +68,42 @@ const Form = ({ navigation }) => {
                     )}
                     name="seconds"
                 />
-                {errors.content && <Text>This is required</Text>}
+                <Text style={errorMsg}>{errors.seconds ? 'This is required' : ''}</Text>
             </View>
             <View style={GlobalStyle.btnContainer}>
-                <TouchableOpacity style={GlobalStyle.btnStyle} onPress={() => navigation.goBack()}>
-                    <Text style={GlobalStyle.btnTxtStyle}>Go back to the list</Text>
-                </TouchableOpacity>
                 <TouchableOpacity style={GlobalStyle.btnStyle} onPress={handleSubmit(onSubmit)}>
                     <Text style={GlobalStyle.btnTxtStyle}>Add new product</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={GlobalStyle.btnStyle} onPress={() => navigation.goBack()}>
+                    <Text style={GlobalStyle.btnTxtStyle}>Go back to the list</Text>
                 </TouchableOpacity>
             </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    txtInput: {
+        fontSize: 25,
+        lineHeight: 35,
+        paddingTop: 5,
+        paddingBottom: 5,
+        paddingLeft: 10,
+        marginBottom: 5,
+        borderColor: 'black',
+        borderWidth: 1,
+        borderRadius: 10,
+        backgroundColor: '#d4d9db',
+    },
+    label: {
+        fontSize: 20,
+        marginBottom: 5,
+    },
+    errorMsg: {
+        fontSize: 15,
+        color: '#DD1040',
+        marginBottom: 15,
+    }
+});
 
 export default Form;
--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -3,8 +3,9 @@ import { StyleSheet } from "react-native";
 export default StyleSheet.create({
     parentContainer: {
         flex: 1,
+        justifyContent: 'space-between',
         padding: 10,
-        backgroundColor: '#C3D99E'
+        backgroundColor: '#C3D99E',
     },
     btnContainer: {
         marginTop: 5,
@@ -14,7 +15,9 @@ export default StyleSheet.create({
         backgroundColor: '#EE782D',
         color: 'white',
         height: 50,
-        borderRadius: 10
+        borderRadius: 10,
+        marginTop: 5,
+        marginBottom: 5
     },
     btnTxtStyle: {
         color: 'white',
